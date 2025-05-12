7532b3a62bd708947ec78ac4134d3e3f493ff462
Rob Pungello <rob@pdfsystems.com>
2025-05-12 15:02:37 -0400

    Allow backups to be created

--- src/Dtos/Backup.php
+++ src/Dtos/Backup.php
@@ -8,20 +8,12 @@ use Rpungello\SdkClient\DataTransferObject;
 class Backup extends DataTransferObject
 {
     public ?int $id;
-
     public ?Application $application;
-
     public ?string $disk;
-
     public ?string $filename;
-
     public ?string $mime_type;
-
     public ?int $size;
-
-    public array $meta = [];
-
+    public ?array $meta;
     public ?Carbon $created_at;
-
     public ?Carbon $updated_at;
 }
--- src/Repositories/BackupRepository.php
+++ src/Repositories/BackupRepository.php
@@ -3,9 +3,12 @@
 namespace Pdfsystems\BackupSdk\Repositories;
 
 use GuzzleHttp\Exception\GuzzleException;
+use Pdfsystems\BackupSdk\Dtos\Application;
 use Pdfsystems\BackupSdk\Dtos\Backup;
 use Pdfsystems\BackupSdk\Dtos\Pagination\BackupList;
+use Rpungello\SdkClient\SdkClient;
 use Spatie\DataTransferObject\Exceptions\UnknownProperties;
+use SplFileInfo;
 
 class BackupRepository extends Repository
 {
@@ -28,5 +31,37 @@ class BackupRepository extends Repository
     public function find(int $id): Backup
     {
         return $this->client->getDto("api/backups/$id", Backup::class);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function create(Application $application, SplFileInfo $file, array $meta = [], ?string $disk = null): Backup
+    {
+        $body = SdkClient::convertJsonToMultipart([
+            'application_id' => $application->id,
+            'filename' => $file->getFilename(),
+            'size' => $file->getSize(),
+            'mime_type' => $file->getType(),
+            'meta' => json_encode($meta),
+            'disk' => $disk,
+        ]);
+
+        $body[] = [
+            'name' => 'file',
+            'contents' => $file->openFile(),
+            'filename' => $file->getFilename(),
+        ];
+
+        return $this->client->postMultipartAsDto('api/backups', $body, Backup::class);
+    }
+
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     */
+    public function update(Backup $backup): Backup
+    {
+        return $this->client->putDto("api/backups/$backup->id", $backup);
     }
 }
