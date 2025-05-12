e79fb818b4ec84c39466020d4944fd3a70252d41
Rob Pungello <rob@pdfsystems.com>
2025-05-12 15:02:20 -0400

    Fixes for applications

--- src/Repositories/ApplicationRepository.php
+++ src/Repositories/ApplicationRepository.php
@@ -4,7 +4,7 @@ namespace Pdfsystems\BackupSdk\Repositories;
 
 use GuzzleHttp\Exception\GuzzleException;
 use Pdfsystems\BackupSdk\Dtos\Application;
-use Pdfsystems\BackupSdk\Dtos\Pagination\BackupList;
+use Pdfsystems\BackupSdk\Dtos\Pagination\ApplicationList;
 use Spatie\DataTransferObject\Exceptions\UnknownProperties;
 
 class ApplicationRepository extends Repository
@@ -13,12 +13,12 @@ class ApplicationRepository extends Repository
      * @throws UnknownProperties
      * @throws GuzzleException
      */
-    public function list(int $perPage = 15, int $page = 1): BackupList
+    public function list(int $perPage = 15, int $page = 1): ApplicationList
     {
-        return $this->client->getDto('api/applications', BackupList::class, [
-            'count' => $perPage,
-            'page' => $page,
-        ]);
+        return $this->client->getDto('api/applications', ApplicationList::class, [
+                'count' => $perPage,
+                'page' => $page,
+            ]);
     }
 
     /**
@@ -28,5 +28,23 @@ class ApplicationRepository extends Repository
     public function find(int $id): Application
     {
         return $this->client->getDto("api/applications/$id", Application::class);
+    }
+
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     */
+    public function create(Application $application): Application
+    {
+        return $this->client->postDto('api/applications', $application);
+    }
+
+    /**
+     * @throws UnknownProperties
+     * @throws GuzzleException
+     */
+    public function update(Application $application): Application
+    {
+        return $this->client->putDto("api/applications/$application->id", $application);
     }
 }
