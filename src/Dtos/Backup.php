0794e46209993f0207c4c514c5a7597ee7342f45
rpungello <3287663+rpungello@users.noreply.github.com>
2025-05-12 19:04:30 +0000

    Fix styling

--- src/Dtos/Backup.php
+++ src/Dtos/Backup.php
@@ -8,12 +8,20 @@ use Rpungello\SdkClient\DataTransferObject;
 class Backup extends DataTransferObject
 {
     public ?int $id;
+
     public ?Application $application;
+
     public ?string $disk;
+
     public ?string $filename;
+
     public ?string $mime_type;
+
     public ?int $size;
+
     public ?array $meta;
+
     public ?Carbon $created_at;
+
     public ?Carbon $updated_at;
 }
